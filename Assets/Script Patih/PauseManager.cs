668290a290af46db5a8f7b15af0eaa1d245fe027
Udean7789 <183721716+UdinGanteng256@users.noreply.github.com>
2026-01-23 11:34:08 +0700

    sek

---
+++ Assets/Script Patih/PauseManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsPanel;
+
+    void Start()
+    {
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        if (settingsPanel != null)
+            settingsPanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+
+        // Untuk Reset MusicManager
+       // if (MusicManager.Instance != null)
+       // {
+       //     MusicManager.Instance.ResetStateOnRestart();
+       // }
+
+        //if (SceneBGMManager.Instance != null)
+        //{
+        //    SceneBGMManager.Instance.StopBGM();
+        //}
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Home()
+    {
+        Time.timeScale = 1f;
+
+       // if (MusicManager.Instance != null)
+       // {
+       //     MusicManager.Instance.ResetStateOnRestart();
+       // }
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
