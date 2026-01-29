d118ac5964ace1172664c492db88d583f4fef156
Udean7789 <168752465+Udean7789@users.noreply.github.com>
2026-01-29 23:11:13 +0700

    Fix

---
+++ Assets/Script/SceneChanger2.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger2 : MonoBehaviour
+{
+    public float changeTime;
+    public string sceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeTime -= Time.deltaTime;
+        if (changeTime <= 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
